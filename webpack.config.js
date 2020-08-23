b9ae4dc1393f3d35610df1c7fecde26a013a7158
Nam Phung <nphung@macalester.edu>
2020-08-22 21:37:30 -0500

    typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,22 @@
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const DotenvWebpackPlugin = require('dotenv-webpack');
 const path = require('path');
 
-module.exports = {
+module.exports = (env) => ({
+  entry: path.join(__dirname, 'src', 'index.tsx'),
   output: {
     path: path.join(__dirname, 'build'),
+    publicPath: '/',
   },
   module: {
     rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
@@ -24,15 +34,22 @@ module.exports = {
       },
     ],
   },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
   devServer: {
+    contentBase: path.join(__dirname, 'build'),
     port: 3000,
-    compress: true,
     open: false,
+    historyApiFallback: true,
   },
   plugins: [
     new HTMLWebpackPlugin({
       template: path.join(__dirname, 'public', 'index.html'),
       filename: path.join(__dirname, 'build', 'index.html'),
     }),
+    new DotenvWebpackPlugin({
+      path: `./.env/${env.NODE_ENV}.local`,
+    }),
   ],
-};
+});
